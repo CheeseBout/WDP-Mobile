c8434f8cbf14d6d3dfb9be3da081687e4775938d
Lê Nguyễn Chí Bảo <baolncse183721@fpt.edu.vn>
2025-06-03 10:45:58 +0700

    Adjust layout for overlied prevention

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
+import { StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
-    </ClerkProvider>
+    <SafeAreaView style={styles.container}>
+      <ClerkProvider tokenCache={tokenCache} >
+        <Slot />
+      </ClerkProvider>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+})
